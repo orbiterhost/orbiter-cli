df48b65bd881b4160f14c8922ddd0236103fbbdd
Steve Simkins <73185218+stevedylandev@users.noreply.github.com>
2025-01-04 02:15:18 -0500

    chore: refactor config

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-export const SUPABASE_CONFIG = {
-  URL: "https://myyfwiyflnerjrdaoyxs.supabase.co",
-  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15eWZ3aXlmbG5lcmpyZGFveXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NzgwMzMsImV4cCI6MjA1MDU1NDAzM30.lxjcKW5Mwd-Tw3NU7TzmyKhydjEBmBfc0IID1U167XM"
-} as const;
-
-export const API_URL = "https://api.orbiter.host"
