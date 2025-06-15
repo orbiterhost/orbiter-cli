e960094eec123a48209b79b5b818bed53203de9e
Steve <stevedsimkins@gmail.com>
2025-06-15 10:08:57 -0400

    feat: Added functions to bhvr deployment

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ import { createInteractiveMiniApp } from "./utils/miniapp";
 import { createTemplateApp } from "./utils/template";
 import Univers from "./fonts/Univers.js";
 
-figlet.parseFont('Univers', Univers);
+figlet.parseFont("Univers", Univers);
 
 const text = figlet.textSync("ORBITER", {
 	font: "Univers",
@@ -298,9 +298,16 @@ const createTemplateAppCmd = command({
 			displayName: "name",
 			description: "Name of the new project",
 		}),
+		template: option({
+			type: optional(string),
+			long: "template",
+			short: "t",
+			description: "Optional template you want to use",
+			defaultValue: undefined,
+		}),
 	},
 	handler: async (args) => {
-		await createTemplateApp(args.projectName);
+		await createTemplateApp(args.projectName, args.template);
 	},
 });
 
