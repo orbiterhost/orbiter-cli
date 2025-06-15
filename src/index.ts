962560a1eadf04d573277bbf18becf41cf34f4e9
Steve <stevedsimkins@gmail.com>
2025-06-15 16:48:05 -0400

    chore: version bump

--- src/index.ts
+++ src/index.ts
@@ -313,8 +313,8 @@ const createTemplateAppCmd = command({
 
 const cli = subcommands({
 	name: "orbiter",
-	description: `\n ${text} \n Create and manage static sites with Orbiter. Get started by running orbiter login`,
-	version: "0.9.3",
+	description: `\n ${text} \n Create and manage static sites with Orbiter. Get started by running orbiter auth`,
+	version: "0.9.4",
 	cmds: {
 		login: loginCmd,
 		auth: authCmd,
