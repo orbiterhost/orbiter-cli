41fa6e2dca955d3f2fb38390a48e3dd9e7ff484f
Steve Simkins <stevedsimkins@gmail.com>
2025-07-21 16:19:51 -0400

    chore: version bump

--- src/index.ts
+++ src/index.ts
@@ -370,7 +370,7 @@ const deployServerCmd = command({
 const cli = subcommands({
 	name: "orbiter",
 	description: `\n ${text} \n Create and manage static sites with Orbiter. Get started by running orbiter auth`,
-	version: "0.9.7",
+	version: "0.9.8",
 	cmds: {
 		login: loginCmd,
 		auth: authCmd,
